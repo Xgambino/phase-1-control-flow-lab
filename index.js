fe7de08bf98ee412fe1272912b08a53dd237cd6c
Denis <mugambimuthuri0@gmail.com>
2024-03-26 03:58:17 +0300

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance > 400 && distance <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (distance > 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    return "No can do.";
+  } else {
+    return "Distance must be a positive number.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+// Example usage:
+console.log(scuberGreetingForFeet(300));
+console.log(scuberGreetingForFeet(2200)); 
+console.log(scuberGreetingForFeet(3000)); 
+console.log(scuberGreetingForFeet(-100)); 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? 'Ok, sounds good.' :
+  'No go.'
 }
+console.log(ternaryCheckCity("NYC"))
+console.log(ternaryCheckCity("Tokyo"))
+
+function switchOnCharmFromTip(tipAmount) {
+  switch (tipAmount) {
+      case 'generous':
+          return "Thank you so much.";
+      case 'not as generous':
+          return "Thank you."; 
+      default:
+          return "Bye.";
+  }
+}
+console.log(switchOnCharmFromTip('generous'))
